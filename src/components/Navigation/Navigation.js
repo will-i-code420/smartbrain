548ef7c0b80e05e6f5b23e8895fab4d8d54b5653
Will Aronow <waronow123@gmail.com>
2021-10-18 15:21:32 -0700

    made nav functional component

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,23 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { TabMenu } from 'primereact/tabmenu';
 
-const Navigation = () => {
-	const items = [
-		{ label: 'Home', icon: 'pi pi-fw pi-home' },
-		{ label: 'Calendar', icon: 'pi pi-fw pi-calendar' },
-		{ label: 'Edit', icon: 'pi pi-fw pi-pencil' },
-		{ label: 'Documentation', icon: 'pi pi-fw pi-file' },
-		{ label: 'Settings', icon: 'pi pi-fw pi-cog' }
-	];
-	let activeIndex = 0;
-	const setActiveIndex = (i) => {
-		activeIndex = i;
+class Navigation extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			items: [
+				{ label: 'Home', icon: 'pi pi-fw pi-home' },
+				{ label: 'Calendar', icon: 'pi pi-fw pi-calendar' },
+				{ label: 'Edit', icon: 'pi pi-fw pi-pencil' },
+				{ label: 'Documentation', icon: 'pi pi-fw pi-file' },
+				{ label: 'Settings', icon: 'pi pi-fw pi-cog' }
+			],
+			activeIndex: 0
+		};
+	}
+	setActiveIndex = (i) => {
+		this.setState({ activeIndex: i });
 	};
-	return (
-		<nav>
-			<TabMenu model={items} activeIndex={activeIndex} onTabChange={(e) => setActiveIndex(e.value)} />
-		</nav>
-	);
-};
+	render() {
+		const { items, activeIndex } = this.state;
+		return (
+			<nav>
+				<TabMenu model={items} activeIndex={activeIndex} onTabChange={(e) => this.setActiveIndex(e.value)} />
+			</nav>
+		);
+	}
+}
 
 export default Navigation;
