caa349facf64e203c00b948533df444f3524571f
Will Aronow <waronow123@gmail.com>
2021-10-18 15:38:33 -0700

    added brain logo

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import Tilty from 'react-tilty';
+import brain from './brain.png';
 
 const Logo = () => {
 	return (
 		<div>
 			<Tilty style={{ transformStyle: 'preserve-3d' }}>
-				<div style={{ transform: 'translateZ(30px)' }}>Logo Here</div>
+				<div style={{ transform: 'translateZ(30px)' }}>
+					<img src={brain} alt="SmartBrain Logo" />
+				</div>
 			</Tilty>
 		</div>
 	);
